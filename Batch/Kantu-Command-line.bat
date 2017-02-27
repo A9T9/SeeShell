9cb72eae6879df11149ce728e183f06fe76a276a
a9t9 <github@a9t9.com>
2017-02-27 15:35:12 +0100

    Update Kantu-Command-line.bat

--- Batch/Kantu-Command-line.bat
+++ Batch/Kantu-Command-line.bat
@@ -8,7 +8,7 @@ Rem The -ae switch tells Kantu to close after the script has been run
 
 "C:\Program Files (x86)\Kantu\kantu.exe" C:\Users\[YOUR USER HERE]\Documents\Kantu\scripts\demo-selectbox.xkts -ae
 
-
+ 
 Rem If the batch file should not wait for Kantu to complete the script and exit then use START "" 
 Rem start "" "C:\Program Files (x86)\Kantu\kantu.exe" C:\Users\m\Documents\Kantu\scripts\demo-contactus.xkts -ae
 
