f54b08741c02405a8094aae894669eb44cbc83cf
a9t9 <github@a9t9.com>
2017-04-15 00:55:32 +0200

    v2

--- Batch/Kantu-Command-line.bat
+++ Batch/Kantu-Command-line.bat
@@ -4,9 +4,9 @@ Rem You need to change them to work for your installation
 Rem The -ae switch tells Kantu to close after the script has been run
 
 
-"C:\Program Files (x86)\Kantu\kantu.exe" C:\Users\[YOUR USER HERE]\Documents\Kantu\scripts\demo-contactus.xkts -ae
+"C:\Program Files (x86)\Kantu\kantu.exe" C:\Users\[YOUR USER HERE]\Documents\Kantu\scripts\macro.kmacro -ae
 
-"C:\Program Files (x86)\Kantu\kantu.exe" C:\Users\[YOUR USER HERE]\Documents\Kantu\scripts\demo-selectbox.xkts -ae
+"C:\Program Files (x86)\Kantu\kantu.exe" C:\Users\[YOUR USER HERE]\Documents\Kantu\scripts\macro.kmacro -ae
 
  
 Rem If the batch file should not wait for Kantu to complete the script and exit then use START "" 
