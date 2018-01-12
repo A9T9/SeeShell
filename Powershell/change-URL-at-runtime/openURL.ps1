f7cf4f116cbca2472cbd93e359f8144a38ebd97f
a9t9 <github@a9t9.com>
2018-01-12 13:26:07 +0100

    open URL at runtime from variable

---
+++ Powershell/change-URL-at-runtime/openURL.ps1
@@ -0,0 +1,40 @@
+#
+# This script sends the URL to use to the Kantu Browser
+# by using setVariable
+#
+# Inside the macro we have: OPEN {myURL}
+
+
+Write-Host "Hello Kantu User!"
+
+$url = "https://a9t9.com"
+
+$comObj = new-object -ComObject Kantu
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+[System.Windows.Forms.MessageBox]::Show("Click OK to start Kantu")
+
+#30 in open (30) is the max time to wait for Kantu to start.
+#If Kantu would take longer (e. g. very high CPU load) then the open command returns a timeout error
+
+$comObj.open(30)
+
+    $comObj.Echo("URL = " + $url)
+
+    $iret = $comObj.setVariable("myURL", $url ) 
+    $iret = $comObj.Play("open-URL", 300)
+
+    #Check test return value
+    If ($iret -eq 1 ) {
+        $sret = "Macro completed OK (" +$iret + ")"
+    }else{
+        $sret = $comObj.GetLastError() + " (" +$iret + ")"
+    }
+
+
+    [System.Windows.Forms.MessageBox]::Show($sret)
+
+    $sdate = Get-Date -Format g
+
+
+    $comObj.Close(10)
